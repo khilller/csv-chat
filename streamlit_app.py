10d6ec9c9e96fab876a5aa2cfc27badcc25014e6
theArchitect <akhil.hem@outlook.com>
2023-12-05 16:00:57 +0000

    openai key

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,8 @@ from langchain.llms import OpenAI
 from dotenv import load_dotenv
 import warnings
 
-openai_key = os.getenv("OPENAI_KEY")
+#openai_key = os.getenv("OPENAI_KEY")
+openai_key = st.secrets("OPENAI_API_KEY")
 
 def main():
 
